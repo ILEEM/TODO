b2af06300c681edc7abbdf27c32416e681d44916
ILEEM <ileem.127.0.0.1@gmail.com>
2024-06-14 03:39:55 +0530

    Added Functions to App

--- TODO/urls.py
+++ TODO/urls.py
@@ -20,5 +20,8 @@ from todoapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home),
+    path('',views.home,name='home'),
+    path('completed/<int:id>/',views.complete_todo,name='complete_todo'),
+    path('delete/<int:id>/',views.delete_todo,name='delete_todo'),
+    path('edit/<int:id>/',views.edit_todo,name='edit_todo'),
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,7 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+
+from todoapp.forms import TodoForm
 from .models import Todo
 
 # Create your views here.
 def home(request):
-    todos = Todo.objects.all()
-    return render(request,'todo_home.html',{'todos':todos})
+    todos = Todo.objects.filter(Status=False)
+    completed = Todo.objects.filter(Status=True)
+    if request.method == "POST":
+        task = request.POST.get('task')
+        desc = request.POST.get('description')
+        priority = request.POST.get('priority')
+        new_todo = Todo(Task=task,Description=desc,Priority=priority)
+        new_todo.save()
+    return render(request,'todo_home.html',{'todos':todos,'completed':completed})
+
+def complete_todo(request, id):
+    todo = get_object_or_404(Todo,id=id)
+    todo.Status = True
+    todo.save()
+    return redirect('home')
+
+def delete_todo(request, id):
+    todo = get_object_or_404(Todo,id=id)
+    todo.delete()
+    return redirect('home')
+
+def edit_todo(request, id):
+    todo = get_object_or_404(Todo, id=id)
+    if request.method == 'POST':
+        todo.Task = request.POST.get('task')
+        todo.Description = request.POST.get('description')
+        todo.Priority = request.POST.get('priority')
+        todo.save()
+        return redirect('home')        
+    return render(request, 'edit_todo.html', {'todo': todo})
